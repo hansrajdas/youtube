e37f1dbea850f7ba7274f402c26f8c67a4c29505
Hansraj Das <hansrajd@vmware.com>
2022-08-08 01:13:39 +0530

    lc-278

---
+++ problem-solving/_lc-278-first-bad-version/iterative.py
@@ -0,0 +1,23 @@
+"""
+https://leetcode.com/problems/first-bad-version/
+
+Complexity
+----------
+Time: O(logn)
+Space: O(1)
+"""
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
