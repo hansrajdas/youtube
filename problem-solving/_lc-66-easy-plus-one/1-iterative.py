039a6d2d90066a4f77c4c4f68d89c4a9e93d3cbf
Hansraj Das <hansrajd@vmware.com>
2022-08-11 15:44:08 +0530

    lc-66

--- problem-solving/_lc-66-easy-plus-one/1-iterative.py
+++ problem-solving/_lc-66-easy-plus-one/1-iterative.py
@@ -9,8 +9,7 @@ Space: (1)
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
-        for i in range(n - 1, -1, -1):
+        for i in range(len(digits) - 1, -1, -1):
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
