a733b9c8f1df78126ea725f1154fe34dd7c811f8
Hansraj Das <hansrajd@vmware.com>
2022-08-13 17:32:57 +0530

    lc-100

---
+++ problem-solving/_lc-100-easy-same-tree/2-iterative.py
@@ -0,0 +1,26 @@
+"""
+https://leetcode.com/problems/same-tree/
+
+
+Complexity
+----------
+Time and Space: O(N)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        stack = [(p, q)]
+        while stack:
+            n1, n2 = stack.pop()
+            if n1 and n2 and n1.val == n2.val:
+                stack.append((n1.left, n2.left))
+                stack.append((n1.right, n2.right))
+            elif n1 != n2:
+                return False
+        return True
