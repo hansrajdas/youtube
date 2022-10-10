973c9db3cf4b88a4258443324c8287966111b8f2
Hansraj Das <hansrajd@vmware.com>
2022-10-11 01:01:50 +0530

    Revert "lc-174"
    This reverts commit 559a4aa88e7a35d8f22f77db960c1f6a42f048b3.

--- problem-solving/create-dir.sh
+++ problem-solving/create-dir.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -x
-export num=174
-export level=hard
-export problem="dungeon-game"
+export num=221
+export level=medium
+export problem="maximal-square"
 
 mkdir _lc-$num-$level-$problem
