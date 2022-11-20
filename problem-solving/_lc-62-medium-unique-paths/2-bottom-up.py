96d3ff9247c1657c5c4050fc18a0965c311b9320
Hansraj Das <hansrajd@vmware.com>
2022-11-21 00:44:55 +0530

    lc-62

--- problem-solving/_lc-62-medium-unique-paths/2-bottom-up.py
+++ problem-solving/_lc-62-medium-unique-paths/2-bottom-up.py
@@ -9,8 +9,11 @@ Space: O(m*n)
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1] * (n + 1) for _ in range(m + 1)]
-        for r in range(2, m + 1):
-            for c in range(2, n + 1):
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for r in range(1, m + 1):
+            for c in range(1, n + 1):
+                if r == 1 or c == 1:
+                    dp[r][c] = 1
+                    continue
                 dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
         return dp[m][n]
--- problem-solving/_lc-62-medium-unique-paths/3-bottom-up-space-optimized.py
+++ problem-solving/_lc-62-medium-unique-paths/3-bottom-up-space-optimized.py
@@ -9,10 +9,13 @@ Space: O(n)
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        curr = [1] * (n + 1)
+        curr = [0] * (n + 1)
         prev = curr[:]
-        for r in range(2, m + 1):
-            for c in range(2, n + 1):
+        for r in range(1, m + 1):
+            for c in range(1, n + 1):
+                if r == 1 or c == 1:
+                    curr[c] = 1
+                    continue
                 curr[c] = prev[c] + curr[c - 1]
             prev = curr[:]
         return curr[n]
