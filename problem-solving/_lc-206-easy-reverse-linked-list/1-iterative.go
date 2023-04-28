496dd93f2a7cd7ace4a5eee31f37f147e9250262
Hansraj Das <hansrajd@vmware.com>
2023-04-29 01:23:53 +0530

    lc-206

---
+++ problem-solving/_lc-206-easy-reverse-linked-list/1-iterative.go
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ * 
+ * Complexity
+ * ----------
+ * Time: O(n)
+ * Space: O(1)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    curr := head
+    next := head
+    for curr != nil {
+        next = next.Next
+        curr.Next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+}
---
+++ problem-solving/_lc-206-easy-reverse-linked-list/2-recursive.go
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ * 
+ * Complexity
+ * ----------
+ * Time and space: O(n)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverse(prev, curr *ListNode) *ListNode {
+    if curr == nil {
+        return prev
+    }
+    newHead := reverse(curr, curr.Next)
+    curr.Next = prev
+    return newHead
+}
+
+func reverseList(head *ListNode) *ListNode {
+    return reverse(nil, head)    
+}
