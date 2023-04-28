20bfc9a7d9696239c8e9d31b6cfd81b46abffa0b
Hansraj Das <hansrajd@vmware.com>
2023-04-29 01:35:57 +0530

    lc-1

---
+++ problem-solving/lc-1-easy-two-sum/1-brute-force.go
@@ -0,0 +1,19 @@
+/**
+ * Problem: https://leetcode.com/problems/two-sum/
+ * 
+ * Complexity
+ * ----------
+ * Time: O(N^2)
+ * Space: O(1)
+ */
+
+func twoSum(nums []int, target int) []int {
+    for i := 0; i < len(nums); i++ {
+        for j := i + 1; j < len(nums); j++ {
+            if nums[i] + nums[j] == target {
+                return []int{i, j}
+            }
+        }
+    }
+    return []int{}
+}
---
+++ problem-solving/lc-1-easy-two-sum/2-optimized.go
@@ -0,0 +1,19 @@
+/**
+ * Problem: https://leetcode.com/problems/two-sum/
+ * 
+ * Complexity
+ * ----------
+ * Time: O(N)
+ * Space: O(N)
+ */
+
+func twoSum(nums []int, target int) []int {
+    seen := make(map[int]int)
+    for i, n := range nums {
+        if idx, ok := seen[target - n]; ok {
+            return []int{i, idx}
+        }
+        seen[n] = i
+    }
+    return []int{}
+}
