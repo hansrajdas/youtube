22071e4bb4c2e5c3ba2a29b5fb60d2f5676b73a4
Hansraj Das <hansrajd@vmware.com>
2023-04-29 01:32:39 +0530

    lc-231

---
+++ problem-solving/_lc-231-easy-power-of-two/1-looping.go
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/power-of-two/
+ * 
+ * Complexity
+ * ----------
+ * Time: O(logn)
+ */
+
+func isPowerOfTwo(n int) bool {
+    if n < 1 {
+        return false
+    }
+    for n % 2 != 1 {
+        n /= 2
+    }
+    return n == 1
+}
---
+++ problem-solving/_lc-231-easy-power-of-two/2-binary-operation.go
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/power-of-two/
+ * 
+ * Complexity
+ * ----------
+ * Time: O(1)
+ */
+
+func isPowerOfTwo(n int) bool {
+    return n != 0 && n & (n - 1) == 0
+}
