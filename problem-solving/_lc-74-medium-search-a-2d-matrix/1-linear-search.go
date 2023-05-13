20a103ae8d7754349c4af7fc6b5df6b94809d79f
Hansraj Das <hansrajd@vmware.com>
2023-05-13 18:07:43 +0530

    lc-74

---
+++ problem-solving/_lc-74-medium-search-a-2d-matrix/1-linear-search.go
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/search-a-2d-matrix/
+ * 
+ * Complexity
+ * ----------
+ * Time: O(rows + cols)
+ * Space: O(1)
+ */
+
+func searchMatrix(matrix [][]int, target int) bool {
+    r := 0
+    c := len(matrix[0]) - 1
+    for r < len(matrix) && c >= 0 {
+        if matrix[r][c] == target {
+            return true
+        } else if matrix[r][c] < target {
+            r += 1
+        } else {
+            c -= 1
+        }
+    }
+    return false
+}
---
+++ problem-solving/_lc-74-medium-search-a-2d-matrix/2-binary-search.go
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/search-a-2d-matrix/
+ * 
+ * Complexity
+ * ----------
+ * Time: O(log(rows*cols)
+ * Space: O(1)
+ */
+
+func searchMatrix(matrix [][]int, target int) bool {
+    rows := len(matrix)
+    cols := len(matrix[0])
+    left := 0
+    right := rows * cols - 1
+    for left <= right {
+        mid := (left + right) / 2
+        r := mid / cols
+        c := mid % cols
+        if matrix[r][c] == target {
+            return true
+        } else if matrix[r][c] < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return false
+}
