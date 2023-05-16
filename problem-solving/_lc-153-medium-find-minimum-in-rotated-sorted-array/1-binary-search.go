0e73be444b8a1e5bff4304ef39068db713bce44b
Hansraj Das <hansrajd@vmware.com>
2023-05-17 00:35:57 +0530

    lc-153

--- problem-solving/_lc-153-medium-find-minimum-in-rotated-sorted-array/1-binary-search.go
+++ problem-solving/_lc-153-medium-find-minimum-in-rotated-sorted-array/1-binary-search.go
@@ -3,23 +3,19 @@ https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
 Complexity
 ----------
-Time:
-  Best case: O(logn)
-  Worst case: O(n)
+Time: O(logn)
 Space: O(1)
 */
 
 func findMin(nums []int) int {
     left, right := 0, len(nums) - 1
-    for left <= right {
+    for left < right {
         mid := (left + right) / 2
-        if mid + 1 < len(nums) && nums[mid] > nums[mid + 1] {
-            return nums[mid + 1]
-        } else if nums[left] <= nums[mid] {
+        if nums[mid] > nums[right] {
             left = mid + 1
         } else {
-            right = mid - 1
+            right = mid
         }
     }
-    return nums[0]
+    return nums[left]
 }
--- problem-solving/_lc-153-medium-find-minimum-in-rotated-sorted-array/1-binary-search.py
+++ problem-solving/_lc-153-medium-find-minimum-in-rotated-sorted-array/1-binary-search.py
@@ -3,21 +3,17 @@ https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
 Complexity
 ----------
-Time:
-  Best case: O(logn)
-  Worst case: O(n)
+Time: O(logn)
 Space: O(1)
 """
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
-        while left <= right:
+        while left < right:
             mid = (left + right) // 2
-            if mid + 1 < len(nums) and nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]
-            elif nums[left] <= nums[mid]:
+            if nums[mid] > nums[right]:
                 left = mid + 1
             else:
-                right = mid - 1
-        return nums[0]
+                right = mid
+        return nums[left]
